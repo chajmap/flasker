99cd1af74c4ff3946e88d2b5af7a638848e0954d
Petr Chajma <chajmap@fzp.czu.cz>
2024-11-08 15:51:44 +0100

    what the forms

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] ="my super secret key"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route("/")
 
@@ -14,6 +23,16 @@ def index():
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html",user_name=name)
+
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = " "
+    return render_template("name.html", name = name, form = form)
 
 @app.errorhandler(404)
 def page_not_found(e):
