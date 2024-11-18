cc5b756ed09c67754bbe7ae5003649e457328071
Petr Chajma <chajmap@fzp.czu.cz>
2024-11-18 13:09:22 +0100

    blog

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.widgets import TextArea
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -18,6 +19,47 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root@/users"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Create a Blog Post model
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    author = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    slug = db.Column(db.String(255))
+
+# Create a Posts Form
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = StringField("Content", validators=[DataRequired()], widget=TextArea())
+    author = StringField("Author", validators=[DataRequired()])
+    slug = StringField("Slug", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+# Add Post Page
+@app.route("/add-post", methods=["GET","POST"])
+def add_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post = Posts(title=form.title.data, content=form.content.data, author=form.author.data,
+                     slug=form.slug.data)
+        # Clear The Form
+        form.title.data = ""
+        form.content.data = ""
+        form.author.data = ""
+        form.slug.data = ""
+
+        # Add post data to db
+        db.session.add(post)
+        db.session.commit()
+
+        # Return a message
+        flash("Blog Post Submitted Successfully!")
+
+    # Redirect to the webpage
+    return render_template("add_post.html",form=form)
+        
 # Create the model
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,6 +67,20 @@ class Users(db.Model):
     email = db.Column(db.String(120), nullable=False, unique=True)
     institution = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    # Passwords
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError("password is not a readable attribute")
+    
+    @ password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     # Create a string
     def __repr__(self):
         return "<Name %r>" % self.name
@@ -34,6 +90,10 @@ class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     institution = StringField("Institution")
+    password_hash = PasswordField("Password", validators=[DataRequired(),
+                                                           EqualTo("password_hash2",
+                                                                    message="Passwords must match!")])
+    password_hash2 = PasswordField("Confirm Password", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 # Update Database Record
@@ -89,6 +149,12 @@ def delete(id):
 #        db.create_all()
 
 # Create a Form Class
+class PasswordForm(FlaskForm):
+    email = StringField("What's Your Email", validators=[DataRequired()])
+    password_hash = PasswordField("What's Your Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+# Create a Form Class
 class NamerForm(FlaskForm):
     name = StringField("What's Your Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
@@ -100,13 +166,19 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data,institution=form.institution.data)
+            # hash the password
+            hashed_pw=generate_password_hash(form.password_hash.data, "pbkdf2:sha256")
+            user = Users(name=form.name.data, 
+                         email=form.email.data,
+                         institution=form.institution.data, 
+                         password_hash=hashed_pw)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ""
         form.email.data = ""
         form.institution.data = ""
+        form.password_hash.data = ""
         flash("User Added Successfully!")
     our_users = Users.query.order_by(Users.date_added)
     return render_template("add_user.html", 
@@ -145,6 +217,34 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create Password Test Page
+@app.route("/test_pw", methods=["GET", "POST"])
+def test_pw():
+    email = None
+    password = None
+    pw_to_check = None
+    passed = None
+    form = PasswordForm()
+
+    # Validate Form
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password_hash.data
+        form.email.data = " "
+        form.password_hash.data = " "
+        # Lookup user
+        pw_to_check = Users.query.filter_by(email=email).first()
+        # Check hash pw
+        passed = check_password_hash(pw_to_check.password_hash,
+                            password)
+
+    return render_template("test_pw.html", 
+                           email = email, 
+                           password = password,
+                           pw_to_check = pw_to_check,
+                           passed = passed,
+                           form = form)
 
 
 if __name__== "__main__":
