0667cdf5099aa1eb1f5ceca3e0f28fb208eeb894
Petr Chajma <chajmap@fzp.czu.cz>
2024-11-22 18:39:42 +0100

    Dashboard

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, EqualTo, Length
@@ -7,6 +7,7 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 # Flask instance
 app = Flask(__name__)
@@ -18,6 +19,58 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root@/users"
 # Initialize the database
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Flask Login Stuff
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+
+
+# Create a Login Form
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField()
+
+# Create a Login Page
+@app.route("/login", methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form.username.data).first()
+        if user:
+            # check the hash
+            if check_password_hash(user.password_hash, form.password.data):
+                login_user(user)
+                flash("Login Successful!")
+                return redirect(url_for("dashboard"))
+            else:
+                flash("Wrong Password, Try Again!")
+        else:
+            flash("User doesn't exist, Try Again!")
+
+    return render_template("login.html", form=form)
+
+# Create a Logout Page
+@app.route("/logout", methods=["GET","POST"])
+@login_required
+def logout():
+    logout_user()
+    flash("You Have Been Logged Out!")
+    return redirect(url_for("login"))
+
+
+# Create a Dashboard Page
+@app.route("/dashboard", methods=["GET","POST"])
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
 
 # Create a Blog Post model
 class Posts(db.Model):
@@ -35,6 +88,56 @@ class PostForm(FlaskForm):
     author = StringField("Author", validators=[DataRequired()])
     slug = StringField("Slug", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+@app.route("/posts/delete/<int:id>")
+def delete_post(id):
+    post_to_delete = Posts.query.get_or_404(id)
+    try:
+        db.session.delete(post_to_delete)
+        db.session.commit()
+
+        flash("Blog Post Was Deleted!") 
+
+        posts = Posts.query.order_by(Posts.date_posted)   
+        return render_template("posts.html", posts=posts)
+    
+    except:
+        flash("There was a problem deleting the post. Please, try again!")
+        
+        posts = Posts.query.order_by(Posts.date_posted)   
+        return render_template("posts.html", posts=posts)
+
+
+@app.route("/posts")
+def posts():
+    # Grab all the posts
+    posts = Posts.query.order_by(Posts.date_posted)
+    return render_template("posts.html", posts=posts)
+
+@app.route("/posts/<int:id>")
+def post(id):
+    post = Posts.query.get_or_404(id)
+    return render_template("post.html", post=post)
+
+@app.route("/posts/edit/<int:id>", methods=["GET","POST"])
+def edit_post(id):
+    post = Posts.query.get_or_404(id)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.author = form.author.data
+        post.slug = form.slug.data
+        post.author = form.author.data
+        # Update db
+        db.session.add(post)
+        db.session.commit()
+        flash("Post has Been Updated!")
+        return redirect(url_for("post", id=post.id))
+    form.title.data = post.title
+    form.author.data = post.author
+    form.slug.data = post.slug
+    form.content.data = post.content
+    return render_template("edit_post.html", form=form)
 
 # Add Post Page
 @app.route("/add-post", methods=["GET","POST"])
@@ -61,8 +164,9 @@ def add_post():
     return render_template("add_post.html",form=form)
         
 # Create the model
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     institution = db.Column(db.String(120))
@@ -70,7 +174,7 @@ class Users(db.Model):
     # Passwords
     password_hash = db.Column(db.String(128))
 
-    @property
+    @ property
     def password(self):
         raise AttributeError("password is not a readable attribute")
     
@@ -88,6 +192,7 @@ class Users(db.Model):
 # Create a Form Class
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     institution = StringField("Institution")
     password_hash = PasswordField("Password", validators=[DataRequired(),
@@ -169,6 +274,7 @@ def add_user():
             # hash the password
             hashed_pw=generate_password_hash(form.password_hash.data, "pbkdf2:sha256")
             user = Users(name=form.name.data, 
+                         username=form.username.data,
                          email=form.email.data,
                          institution=form.institution.data, 
                          password_hash=hashed_pw)
@@ -176,6 +282,7 @@ def add_user():
             db.session.commit()
         name = form.name.data
         form.name.data = ""
+        form.username.data = ""
         form.email.data = ""
         form.institution.data = ""
         form.password_hash.data = ""
